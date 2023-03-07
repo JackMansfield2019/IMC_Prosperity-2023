9addd0e3630d17d340bcf95b8c42286566335aa9
RyancRiv <104662361+RyancRiv@users.noreply.github.com>
2023-03-07 14:23:15 -0500

    Add files via upload

---
+++ Get_data.py
@@ -0,0 +1,30 @@
+# The Python code below is the minimum code that is required in a submission file:
+# 1. The "datamodel" imports at the top. Using the typing library is optional.
+# 2. A class called "Trader", this class name should not be changed.
+# 3. A run function that takes a tradingstate as input and outputs a "result" dict.
+
+import pandas
+import numpy
+import statistics
+import math
+import typing
+
+from typing import Dict, List
+from datamodel import OrderDepth, TradingState, Order
+
+
+class Trader:
+
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        """
+        Takes all buy and sell orders for all symbols as an input,
+        and outputs a list of orders to be sent
+        """
+        # print(state.market_trades)
+        temp_dict = state.market_trades
+        for key in temp_dict:
+            for trade in temp_dict[key]:
+                print(trade)
+
+        result = {}
+        return
