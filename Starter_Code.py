73931d77618e48e48965d8a23660264f445e9626
ecco257 <connorjwittman@gmail.com>
2023-03-07 17:46:36 -0500

    Add sort order depth, VWAP/Total Value of market order

--- Starter_Code.py
+++ Starter_Code.py
@@ -54,6 +54,106 @@ def getBuySellVol(order_depth: OrderDepth, buy: bool) -> int:
 
     return total
 
+def sortOrderDepth(order_depth: OrderDepth) -> None:
+    """
+    Sorts the buy_orders in the order depth in descending order and the sell_orders in ascending order
+    
+    Parameters:
+    order_depth (OrderDepth): The order depth to sort
+    
+    Returns:
+    None
+    """
+    buy_keys = list(order_depth.buy_orders.keys())
+    buy_keys.sort(reverse=True)
+    sorted_buy_orders = {i : order_depth.buy_orders[i] for i in buy_keys}
+
+    sell_keys = list(order_depth.sell_orders.keys())
+    sell_keys.sort()
+    sorted_sell_orders = {i : order_depth.sell_orders[i] for i in sell_keys}
+
+    order_depth.buy_orders = sorted_buy_orders
+    order_depth.sell_orders = sorted_sell_orders
+
+def getMarketActionValue(order_depth: OrderDepth, quantity: int, buy: bool) -> int:
+    """
+    Calculates the total value of a buy or sell order if it were to be executed at the market price
+    
+    Parameters:
+    order_depth (OrderDepth): The order depth to use
+    quantity (int): The quantity of the order
+    buy (bool): Whether the order is a buy or sell order
+    
+    Returns:
+    int: The total value of the market action
+    """
+
+    sortOrderDepth(order_depth)
+
+    if buy:
+        sell_orders = order_depth.sell_orders
+        total_value = 0
+        volume = 0
+        for price in sell_orders:
+            volume += -1 * sell_orders[price]
+            if volume >= quantity:
+                remaining_volume = quantity - (volume - (-1 * sell_orders[price]))
+                total_value += price * remaining_volume
+                break
+            else:
+                total_value += price * (-1 * sell_orders[price])
+    else:
+        buy_orders = order_depth.buy_orders
+        total_value = 0
+        volume = 0
+        for price in buy_orders:
+            volume += buy_orders[price]
+            if volume >= quantity:
+                remaining_volume = quantity - (volume - buy_orders[price])
+                total_value += price * remaining_volume
+                break
+            else:
+                total_value += price * buy_orders[price]
+    
+    return total_value
+
+def getMarketActionVWAP(order_depth: OrderDepth, quantity: int, buy: bool) -> float:
+    """
+    Calculates the volume weighted average price of a buy or sell order if it were to be executed at the market price
+    
+    Parameters:
+    order_depth (OrderDepth): The order depth to use
+    quantity (int): The quantity to get the market action value for
+    buy (bool): Whether the market action is a buy or a sell
+    
+    Returns:
+    float: The volume weighted average price of the market action
+    """
+
+    total_value = getMarketActionValue(order_depth, quantity, buy)
+
+    return total_value / quantity
+
+def printOrderDepth(order_depth: OrderDepth) -> None:
+    """
+    Prints the order depth for a symbol
+    
+    Parameters:
+    order_depth (OrderDepth): The order depth to print
+        
+    Returns:
+    None
+    """
+    sortOrderDepth(order_depth)
+
+    print("Buy Orders:")
+    for price in order_depth.buy_orders:
+        print("Price: " + str(price) + " Volume: " + str(order_depth.buy_orders[price]))
+    
+    print("Sell Orders:")
+    for price in order_depth.sell_orders:
+        print("Price: " + str(price) + " Volume: " + str(order_depth.sell_orders[price]))
+
 class Trader:
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
