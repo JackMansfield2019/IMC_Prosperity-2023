9f6ec4a8c155365a9403be26549bb69562f9c245
ecco257 <connorjwittman@gmail.com>
2023-03-17 17:37:54 -0400

    Fix adding data for correlation matrix

--- Data_Infrastructure/Correlations/Correlation_Matrix.py
+++ Data_Infrastructure/Correlations/Correlation_Matrix.py
@@ -2,13 +2,21 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data_test1.csv", sep=";")
+df = pd.read_csv("data_test2.csv", sep=";")
 
 #create augmented dataframe (commodities w/ their corresponding mid prices)
 df_aug = pd.DataFrame(columns=["PEARLS", "BANANAS"])
-for idx in range(0, len(df.axes[0]), 2):
-    df_aug.loc[idx, "PEARLS"] = df["mid_price"][idx]
-    df_aug.loc[idx, "BANANAS"] = df["mid_price"][idx+1]
+pearl_index = 0
+banana_index = 0
+for idx in range(0, len(df.axes[0])):
+    if df['product'][idx] == "PEARLS":
+        # add the midprice to the pearls column
+        df_aug.loc[pearl_index, 'PEARLS'] = df['mid_price'][idx]
+        pearl_index += 1
+    else:
+        # add the midprice to the bananas column
+        df_aug.loc[banana_index, 'BANANAS'] = df['mid_price'][idx]
+        banana_index += 1
 
 #correlation matrix computation
 corr = df_aug.astype('float64').corr()
