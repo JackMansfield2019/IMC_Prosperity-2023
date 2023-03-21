21775adc61c2f0e1e8f65f1c7284fe1dd4c111cd
Ethan Girouard <mr.egirouard@gmail.com>
2023-03-21 15:35:43 -0400

    Remove Split_Files

--- Data_Infrastructure/Split_Files/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-# print(d.keys())
-# for fname in d:
-#     print(fname)
-#     # with open(fname, 'w') as f:
-#     #     f.write(d[fname])
-#     #     f.close()
--- Data_Infrastructure/Split_Files/trader_no_orders.py
+++
@@ -1,6 +0,0 @@
-from datamodel import OrderDepth, UserId, TradingState, Order
-
-class Trader:
-    
-    def run(self, state: TradingState):
-     return {}
