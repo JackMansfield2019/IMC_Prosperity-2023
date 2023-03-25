14fa93e6f26dc28482255201c355f1a9d5dc6ae6
ecco257 <connorjwittman@gmail.com>
2023-03-25 15:53:40 -0400

    Update param optimizer to use dotenv for auth token

--- Auto_Uploader/ParamOptimizer.py
+++ Auto_Uploader/ParamOptimizer.py
@@ -1,6 +1,10 @@
 from typing import Any, Dict, Tuple
 from AutoUploader import upload_file, parse_response
 from AutoDownloader import download_algorithms
+import os
+from dotenv import load_dotenv
+#    ^^^^^^
+# pip install python-dotenv
 from random import randint, uniform, gauss
 #                                    ^^^^^
 # this, and possilby other random functions, may be used to get better params
@@ -8,7 +12,11 @@ from random import randint, uniform, gauss
 # information for the upload and download functions
 STRATEGY_PATH = "Hyper_Opt_Strategies/Example_Tunable_Strat.py"
 STRATEGY_NAME = "Example_Tunable_Strat.py"
-AUTH_TOKEN = "eyJraWQiOiJ4M3NhZjFZTkNsRGwyVDljemdCR01ybnVVMlJlNDNjb1E1UGxYMWgwb2tBPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI2MDI3YTg2Ni0zYjUxLTRlZDAtYjYyMy1kNDFhOTQyMWE0YmMiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmV1LXdlc3QtMS5hbWF6b25hd3MuY29tXC9ldS13ZXN0LTFfek9mVngwcWl3IiwiY29nbml0bzp1c2VybmFtZSI6IjYwMjdhODY2LTNiNTEtNGVkMC1iNjIzLWQ0MWE5NDIxYTRiYyIsIm9yaWdpbl9qdGkiOiJjNzI2M2M0NS1jODVkLTRmNWItOTBlZS00MWRiYjk4MGNlNzUiLCJhdWQiOiIzMmM1ZGM1dDFrbDUxZWRjcXYzOWkwcjJzMiIsImV2ZW50X2lkIjoiY2I1NzllNmUtZWZhNy00ZGExLWJjMmUtMzUwNGNmZTkyMjZlIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2Nzk3Mjc0MjgsImV4cCI6MTY3OTczNDYzNCwiaWF0IjoxNjc5NzMxMDM0LCJqdGkiOiIwYTdlNzM2Ny1lMGM2LTQxYTQtODZkZi1jOTcwYjdkMDMxMmMiLCJlbWFpbCI6ImNvbm5vcmp3aXR0bWFuQGdtYWlsLmNvbSJ9.AhVvN0Uawi1LmXW3d1Nj_pVS9_pIPsOECvQpK-3BzWPllKBq4oi08N4TxXs5RZyNqYKEj1k9Oj2pmnAv4-fsjB5JyOV3iD7C1Ta4m7tJgljJ5ywdqMOWINezVIvwNWAISa-iFsWC_5EnPGMq07OdOJLQjqMZJoMcikuz5sFNqyLhEO9MPxxzsfZBGNhZeuUL9-JazNGf7f9ilVaTxcEnR9utp1VhQu4FsjtsTOO9CPnUKg2Z_G8tCfeyQZu_S7sCoOMlsCjYsk2v1PsP4TcG2g1wLiRO6R32dTxEUmjwFN0aFbxouTqaFYdO1PrYSwkyHBajLSAvHEX5Zr_mIDTnPg"
+
+# make a dot env file in this directory called ".env" and put your auth token in it, such as AUTH_TOKEN=0gj20f92efi1093i4jr9qjfqefoidnofieqnfoi
+load_dotenv()
+
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 
 # the limits for the params you want to test
 # the first value is the minimum, the second is the maximum
@@ -73,4 +81,4 @@ def blast_submission(strategy_path: str, strategy_name: str, auth_token: str, nu
     return True
     
 # modify the number of submissions you want to upload here        v
-assert(blast_submission(STRATEGY_PATH, STRATEGY_NAME, AUTH_TOKEN, 3, PARAM_LIMITS))
+assert(blast_submission(STRATEGY_PATH, STRATEGY_NAME, AUTH_TOKEN, 1, PARAM_LIMITS))
